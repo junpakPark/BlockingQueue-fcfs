928ad5a20175d993216b989b9d2fd94e6eeeed2c
junpakPark <112045553+junpakPark@users.noreply.github.com>
2024-03-06 21:14:46 +0900

    feat: BlockingQueue를 이용한 Producer-Consume 패턴 적용

--- src/main/java/com/example/coupon/application/CouponService.java
+++ src/main/java/com/example/coupon/application/CouponService.java
@@ -2,21 +2,41 @@ package com.example.coupon.application;
 
 import com.example.coupon.domain.Coupon;
 import com.example.coupon.domain.CouponRepository;
+import jakarta.annotation.PostConstruct;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicInteger;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 @Service
 public class CouponService {
 
     private final CouponRepository couponRepository;
+    private final BlockingQueue<Coupon> queue = new LinkedBlockingQueue<>();
+
     private final AtomicInteger couponCount = new AtomicInteger();
 
     public CouponService(final CouponRepository couponRepository) {
         this.couponRepository = couponRepository;
     }
 
+    @PostConstruct
     @Transactional
+    public void consume() {
+        new Thread(() -> {
+            while (true) {
+                try {
+                    Coupon coupon = queue.take();
+                    couponRepository.save(coupon);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    break;
+                }
+            }
+        }).start();
+    }
+
+
     public void issue(Long userId) {
         int count = couponCount.incrementAndGet();
 
@@ -24,7 +44,11 @@ public class CouponService {
             throw new IllegalArgumentException("쿠폰 발행갯수를 초과하였습니다.");
         }
 
-        couponRepository.save(new Coupon(userId));
+        try {
+            queue.put(new Coupon(userId));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Transactional(readOnly = true)
